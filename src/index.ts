eebdea3875ae0a02b28dae79f3b66fd20bb4e1ec
GhostProject7688 <164322391+GhostProject7688@users.noreply.github.com>
2024-04-14 18:46:05 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-export * from "./client.js";
+export * from "./client.js"; // main entry Contain All Database Code
+
+
+
+// copyright by GhostProject Studio //
+//
+//
+//
+// status havent on npmjs  Please   //
+// ghost.database package kinda little bug rn

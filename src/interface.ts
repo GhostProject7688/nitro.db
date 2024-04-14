e8c0ad7e393c1472e30e96e16f2bcafb22f7116f
GhostProject7688 <164322391+GhostProject7688@users.noreply.github.com>
2024-04-14 18:44:10 +0700

    Update interface.ts

--- src/interface.ts
+++ src/interface.ts
@@ -1,9 +1,14 @@
 // interface.ts
 
 export interface Database {
-    [key: string]: any;
+    [key: string]: any; // storing Database key or something
 }
 
 export interface Schema {
-    [key: string]: any;
+    [key: string]: any; // schema 
+}
+
+export interface TimeSeriesData { // support timeSeriesData feature
+    timestamp: number;
+    value: any;
 }
